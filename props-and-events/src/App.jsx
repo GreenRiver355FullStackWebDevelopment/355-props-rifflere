3911730584078d84b923904c3f76eab0d38458b4
Rebecca Riffle <rebeccaeriffle@gmail.com>
2025-04-09 15:21:00 -0700

    Added minimal basic styling

--- props-and-events/src/App.jsx
+++ props-and-events/src/App.jsx
@@ -4,6 +4,7 @@ import RestaurantsContainer from "./components/RestaurantsContainer";
 function App() {
   return (
     <div className="App">
+      <h1>Restaurants</h1>
       <RestaurantsContainer />
     </div>
   );
--- props-and-events/src/components/Restaurant.jsx
+++ props-and-events/src/components/Restaurant.jsx
@@ -1,8 +1,17 @@
-function Restaurant() {
+function Restaurant(props) {
 
   return (
     <div className="restaurant">
-      <p>Restaurant Rendered</p>
+
+      <img src={props.restaurant.image} alt={props.restaurant.name} />
+      <h2>{props.restaurant.name}</h2>
+      <ul>
+        <li><em>{props.restaurant.cuisine} Cuisine</em></li>
+        <li>Rating: {props.restaurant.rating}</li>
+        <li>{props.restaurant.address}</li>
+        <li>{props.restaurant.phone}</li>
+      </ul>
+
     </div>
   );
 }
